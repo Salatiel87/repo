1624e002f87185c9bef48b2a9f8c5c1d9200ac06
Guilherme <128631591+Salatiel87@users.noreply.github.com>
2023-03-22 15:17:34 -0300

    Update Caesar_Cipher.py

--- Caesar_Cipher.py
+++ Caesar_Cipher.py
@@ -18,42 +18,42 @@ a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8
 """
 
 def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-      if char in alphabet:
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else:
-      end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}")
+ end_text = ""
+ if cipher_direction == "decode":
+  shift_amount *= -1
+ for char in start_text:
+  if char in alphabet:
+   position = alphabet.index(char)
+   new_position = position + shift_amount
+   end_text += alphabet[new_position]
+  else:
+   end_text += char
+ print(f"Here's the {cipher_direction}d result: {end_text}")
 
 should_end = False
 while not should_end:
-  dir_test = False
-  while not dir_test:
-    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-    if direction == "encode" or direction == "decode":
-      dir_test = True
-    else:
-      print("Invalid option. Type again.")
+ dir_test = False
+ while not dir_test:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+  if direction == "encode" or direction == "decode":
+   dir_test = True
+  else:
+   print("Invalid option. Type again.")
     
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
-  shift = shift % 26
-
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+ text = input("Type your message:\n").lower()
+ shift = int(input("Type the shift number:\n"))
+ shift = shift % 26
+ 
+ caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
   
-  rest_test = False
-  while not rest_test:
-    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
-    if restart == "yes" or restart == "no":
-      rest_test = True
-    else:
-      print("Invalid option. Type again.")
+ rest_test = False
+ while not rest_test:
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+  if restart == "yes" or restart == "no":
+   rest_test = True
+  else:
+   print("Invalid option. Type again.")
   
-  if restart == "no":
-    should_end = True
-    print("Goodbye")
+ if restart == "no":
+  should_end = True
+  print("Goodbye")
